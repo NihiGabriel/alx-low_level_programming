43ed7640a559491970470539491cd2547d0a5c87
NihiGabriel <gabrielnihi@gmail.com>
2022-07-09 06:30:35 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - a function that draws a straight line in
- * the terminal followed by a new line
+ * the terminal. followed by a new line.
  * @n: An input integer
  * Return: Always 0
  */
