bc3c38179e14b75ab3a3ced42478912e608f0709
NihiGabriel <gabrielnihi@gmail.com>
2022-07-15 11:30:19 -0700

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void reverse_array(int *a, int n);
 char _string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
-char *rot13(car *);
+char *rot13(char *);
 void print_number(int n);
