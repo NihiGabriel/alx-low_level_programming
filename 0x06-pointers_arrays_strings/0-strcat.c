536776f8ec6c67c5b4498d739d9978973280fa8c
NihiGabriel <gabrielnihi@gmail.com>
2022-07-15 11:42:44 -0700

    made changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,11 +7,12 @@
  */
 chr *_strcat(char *dest, char *src)
 {
-char *temp = dest;
-while (*dest)
-dest++;
-while (*src);
-*dest++ = *src++;
-*dest = '\0';
-return (temp);
+int d_count = 0, s_count = 0;
+while (dest[d_count] != '\0')
+d_coun++;
+while (src[s_count] != '\0')
+*(dest + d_count) = *(src + s_count);
+d_count++;
+s_cont++;
+return (dest);
 }
